b0b050263c7724fb937ba6c97433728ae864928d
Jacob Merizian <jmerizia@vt.edu>
2022-01-23 23:09:22 -0500

    add setup

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='gptj_colab',
+      version='0.0.1',
+      description='GPT-J for Colab Notebooks',
+      author='Jacob Merizian',
+      url='https://github.com/jmerizia/gptj_colab',
+      packages=[],
+     )
